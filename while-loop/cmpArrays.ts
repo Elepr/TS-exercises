5b74c825cb2a4aaa812baed9041ea4269c948877
Elepr <scotch.1304@gmail.com>
2024-03-04 14:28:42 +0100

    done

--- while-loop/cmpArrays.ts
+++ while-loop/cmpArrays.ts
@@ -1,6 +1,14 @@
 function cmpArrays(arr1: number[], arr2: number[]): boolean {
-    return true
+    if (arr1.length != arr2.length) return false
+
+        let i = 0
+        while (i < arr1.length) {
+            if (arr1[i] != arr2[i]) return false
+            i++
+        }
+        return true
 }
+
 console.log("cmpArrays :")
 console.log("expected output true :", cmpArrays([0], [0]));
 console.log("expected output true :", cmpArrays([12, 7, 5], [12, 7, 5]));
--- while-loop/isNumberPresent.ts
+++ while-loop/isNumberPresent.ts
@@ -7,7 +7,6 @@ function isNumberPresent(numbers: number[], n: number): boolean {
     return false
 }
 
-
 console.log("isNumberPresent :")
 console.log("expected output (true) :", isNumberPresent([1, 2, 3], 1));
 console.log("expected output (true) :", isNumberPresent([1, 2, 3], 2));
