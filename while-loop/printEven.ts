2838dbc8742b778df293d6668a4d029ba35f0b47
Elepr <scotch.1304@gmail.com>
2024-03-04 13:36:55 +0100

    done

--- while-loop/printEven.ts
+++ while-loop/printEven.ts
@@ -1,4 +1,10 @@
-function printEven(numbers: number[]): void {}
+function printEven(numbers: number[]): void {
+    let i = 0
+    while (i < numbers.length) {
+        if (numbers[i] % 2 == 0) console.log(numbers[i])
+        i++
+    }
+}
 
 console.log("printEvent :")
 printEven([13, 23, 12, 45, 22, 48, 66, 100]);
