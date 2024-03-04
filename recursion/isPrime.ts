189eefaf215f758bd4a976fc92c13e2cf778e263
Elepr <scotch.1304@gmail.com>
2024-03-04 15:23:17 +0100

    done

--- recursion/isPrime.ts
+++ recursion/isPrime.ts
@@ -1,7 +1,10 @@
 function isPrime(n: number): boolean {
+    if (n < 2) return false
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false
+    }
     return true
 }
-
 
 console.log("isPrime :")
 console.log("expected output (false) :", isPrime(0));
