2701ee988ec3d958e6390754404e18bc0cf0d8b5
Elepr <scotch.1304@gmail.com>
2024-03-05 12:03:12 +0100

    export fixe

--- recursion/isPrime.ts
+++ recursion/isPrime.ts
@@ -1,20 +1,23 @@
-function isPrime(n: number): boolean {
+export function isPrime(n: number): boolean {
     if (n < 2) return false
     for (let i = 2; i * i <= n; i++) {
         if (n % i == 0) return false
     }
     return true
 }
-export {isPrime}
 
-// console.log("isPrime :")
-// console.log("expected output (false) :", isPrime(0));
-// console.log("expected output (false) :", isPrime(1));
-// console.log("expected output (true) :", isPrime(2));
-// console.log("expected output (true) :", isPrime(3));
-// console.log("expected output (true) :", isPrime(23));
-// console.log("expected output (true) :", isPrime(67));
-// console.log("expected output (false) :", isPrime(69));
-// console.log("expected output (true) :", isPrime(89));
-// console.log("expected output (false) :", isPrime(128));
-// console.log("expected output (true) :", isPrime(9576890767));
+if (typeof module !== "undefined" && !module.parent) {
+console.log("isPrime :")
+console.log("expected output (false) :", isPrime(0));
+console.log("expected output (false) :", isPrime(1));
+console.log("expected output (true) :", isPrime(2));
+console.log("expected output (true) :", isPrime(3));
+console.log("expected output (true) :", isPrime(23));
+console.log("expected output (true) :", isPrime(67));
+console.log("expected output (false) :", isPrime(69));
+console.log("expected output (true) :", isPrime(89));
+console.log("expected output (false) :", isPrime(128));
+console.log("expected output (true) :", isPrime(9576890767));
+console.log("expected output (true) :",
+isPrime(9576890767));
+}
