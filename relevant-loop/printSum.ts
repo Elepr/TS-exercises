6f7ad9a6286ff3d833369ab4554482492b87e7a9
Elepr <scotch.1304@gmail.com>
2024-03-07 15:53:08 +0100

    done

--- relevant-loop/printSum.ts
+++ relevant-loop/printSum.ts
@@ -1,12 +1,31 @@
-function printSum(numbers: number[]): void {
+function printSum(numbers: number[]): void { //here, we have one less line compared a for loop
     let result = 0
-
-    for (let i = 0; i < numbers.length; i++) {
-    result += numbers[i]
-    }
+    numbers.forEach(n => result += n)
     console.log(result)
 }
 
 console.log("printSum :")
 printSum([10, 5, 3]);
+// expected output : 18
+
+function printSumOf(numbers: number[]): void { //here we have the same number of line compared a for loop, the differnce is in the loop condition not really profitable
+    let result = 0
+
+    for (let n of numbers) {
+        result += n
+    }
+    console.log(result)
+}
+
+console.log("printSum :")
+printSumOf([10, 5, 3]);
+// expected output : 18
+
+function printSumReduce(numbers: number[]): void { //this is my favorit method, just 2 line, is clear and give the code more reading and compact
+    let result = numbers.reduce((a, b) => a + b)
+    console.log(result)
+}
+
+console.log("printSum :")
+printSumReduce([10, 5, 3]);
 // expected output : 18
