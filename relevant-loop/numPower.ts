092b0c1e2cb133251dec23b8ce91e3c696ea33f0
Elepr <scotch.1304@gmail.com>
2024-03-08 09:42:53 +0100

    done

--- relevant-loop/numPower.ts
+++ relevant-loop/numPower.ts
@@ -1,10 +1,5 @@
 function numPower(n: number, power: number): number {
-    let result = 1
-
-    for (let i = 0; i < power; i ++) {
-        result*= n
-    }
-    return result
+    return n**power
 }
 
 console.log("numPower :")
@@ -13,3 +8,12 @@ console.log("expected output (1) :", numPower(34, 0));
 console.log("expected output (64) :", numPower(2, 6));
 console.log("expected output (10578455953408) :", numPower(28, 9));
 
+function numPowerPow(n: number, power: number): number {
+    return Math.pow(n, power)
+}
+
+console.log("numPower :")
+console.log("expected output (1) :", numPowerPow(12, 0));
+console.log("expected output (1) :", numPowerPow(34, 0));
+console.log("expected output (64) :", numPowerPow(2, 6));
+console.log("expected output (10578455953408) :", numPowerPow(28, 9));
