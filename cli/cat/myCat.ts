a2e5daaacad1b8a4dd1a33d0f029215656ca545c
Enguerrand <enguerrandlepretre2a@gmail.com>
2024-03-26 18:02:04 +0100

    fix

--- cli/cat/myCat.ts
+++ cli/cat/myCat.ts
@@ -1,9 +1,9 @@
 #! /usr/local/bin/ts-node
 
-import fs from 'node:fs'
+import { readFileSync } from "node:fs"
 
-if (process.argv.length <= 2) {
-    console.error("myCat: error: You must pass at least one file as parameter")
+if (process.argv.length < 3) {
+    process.stderr.write("myCat: error: You must pass at least one file as parameter\n")
     process.exit(1)
 }
 
@@ -11,22 +11,22 @@ let returnCode = 0
 
 for (const path of process.argv.slice(2)) {
     try {
-        const content = fs.readFileSync(path, {encoding: 'utf8'})
+        const content = readFileSync(path, { encoding: "utf8" })
         process.stdout.write(content)
-    } catch(err: any) {
+    } catch (err: any) {
         switch (err.code) {
             case "ENOENT":
-                console.error(`myCat: ${path}: No such file or directory`)
+                process.stderr.write(`myCat: ${path}: No such file or directory\n`)
                 break;
             case "EISDIR":
-                console.error(`myCat: ${path}: Is a directory`)
+                process.stderr.write(`myCat: ${path}: Is a directory\n`)
                 break;
             case "EACCES":
-                console.error(`myCat: ${path}: Permission denied`)
+                process.stderr.write(`myCat: ${path}: Permission denied\n`)
                 break;
 
             default:
-                console.error(`myCat: ${path}: Unknow error:`, err)
+                process.stderr.write(`myCat: ${path}: Unknow error: ${err}\n`)
                 break;
         }
         returnCode = 1
